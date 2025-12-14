198ffd3883078babe7b24f0306b63ec5d7f3f71b
Thiago Oliveira <geovanithiago08@gmail.com>
2025-12-14 19:21:49 -0300

    Atualização

--- api/server.js
+++ api/server.js
@@ -6,6 +6,8 @@
 import express from "express";
 import cors from "cors";
 import { db } from "./conect.js";
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3001;
